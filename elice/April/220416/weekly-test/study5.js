027d1de33f9a3696828a62b076a9fa5cb05eeb11
SeWoong <pso6222@gmail.com>
2022-05-02 20:08:18 +0900

    weekly-test

---
+++ elice/April/220416/weekly-test/study5.js
@@ -0,0 +1,15 @@
+/* 지시사항 1번을 참고하여 코드를 작성하세요. */
+let btn = document.getElementById('btn');
+let game = document.getElementById('game');
+
+btn.addEventListener('click', function () {
+    game.style.display = "none";
+})
+
+
+
+// function hide_game() {
+//     document.getElementById("game").style.display = "none";
+//   }
+  
+//   document.getElementById("btn").addEventListener("click", hide_game);
